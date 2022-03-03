39ce8b414ad0b88afd6215fb8351b289c7d20b36
Brian Teets <bwt17@msn.com>
2022-03-03 15:48:33 -0700

    completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 5;
+    num2 = 7;
+    num3 = 27;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
