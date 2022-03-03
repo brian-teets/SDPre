949531cf163dde6787249840cb5acb242d93f6c7
Brian Teets <bwt17@msn.com>
2022-03-03 16:12:52 -0700

    Objective3Experimentation

---
+++ Labs/Objective3Experimentation.java
@@ -0,0 +1,10 @@
+public class Objective3Experimentation {
+  public static void main(String[] args) {
+    int num6 = 10;
+    num6++;
+    System.out.println(num6);
+
+    int num7 = 10;
+    System.out.println(num7);
+  }
+}
