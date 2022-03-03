2e70e3841cba80e1663edce94a61e23b63d0bbb4
Brian Teets <bwt17@msn.com>
2022-03-03 15:37:59 -0700

    completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brian Teets";
+    int age = 37;
+    String state = "Tennessee";
+
+    // Having completed some of the Codecademy intro to Java course as part of enrollment, I knew that I could concatenate the print statement
+    System.out.println("Name: " + name + " Age: " + age + " BirthState: " + state);
+  }
+}
