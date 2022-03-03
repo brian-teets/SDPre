69e12af463c6fdadab0b07429b797c1298d8ce8f
Brian Teets <bwt17@msn.com>
2022-03-03 15:52:07 -0700

    updated variable values

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -2,9 +2,9 @@ public class Objective2Lab3 {
   public static void main(String[] args) {
     double num1, num2, num3, average;
 
-    num1 = 5;
-    num2 = 7;
-    num3 = 27;
+    num1 = 5.25;
+    num2 = 7.5;
+    num3 = 17.75;
     average = (num1 + num2 + num3) / 3;
 
     System.out.println(average);
