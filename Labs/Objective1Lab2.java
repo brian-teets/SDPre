6eb928f97ccc4da3c92b452080294122a704b1d4
Brian Teets <bwt17@msn.com>
2022-03-03 14:16:08 -0700

    completed Objective1Lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,10 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    System.out.println("----------------");
+    System.out.println("| Grocery List |");
+    System.out.println("| 1. Milk      |");
+    System.out.println("| 2. Eggs      |");
+    System.out.println("| 3. Bread     |");
+    System.out.println("----------------");
+  }
+}
