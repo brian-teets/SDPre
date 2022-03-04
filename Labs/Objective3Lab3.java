63f0daf6220d31311ddab08d91af383686898c27
Brian Teets <bwt17@msn.com>
2022-03-03 18:45:51 -0700

    modified print statement for Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -10,6 +10,10 @@ public class Objective3Lab3 {
     third = 1 + (int)(Math.random() * 6);
 
     // Print the result of the first random integer + second random integer + the third random integer
-    System.out.println(first + second + third);
+    // Originally I had it written as:  System.out.println(first + second + third);
+    // But, I wanted it to print out and look like the expected result from the lab example
+    // which would also print out each of the three random numbers
+    // I then realized that I had to enclose first + second + third inside (), otherwise it would concatenate and print as a string
+    System.out.println(first + " + " + second + " + " + third + " + " + " = " + (first + second + third));
   }
 }
