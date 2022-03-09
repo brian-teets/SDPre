6b5c8c74838034d7b972d3cbbfa405fa6aaf427d
Brian Teets <bwt17@msn.com>
2022-03-09 10:48:08 -0700

    completed Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,10 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;   // counter variable starting value
+
+    while (counter <= 20) {
+      System.out.println(counter);  // print the value of counter with each iteration
+      counter = counter + 1;   // increment the counter
+    }
+  }
+}
