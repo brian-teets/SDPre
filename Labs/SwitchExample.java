b225d5b306e111a41d800a6483588c0716728642
Brian Teets <bwt17@msn.com>
2022-03-09 09:46:56 -0700

    updated SwitchExample to include fall through code

--- Labs/SwitchExample.java
+++ Labs/SwitchExample.java
@@ -1,22 +1,29 @@
 public class SwitchExample {
   public static void main(String[] args) {
     char grade;
-    grade = 'B';
+    grade = 'A';
 
     switch (grade) {
       case 'A':
+      // allow fall through in the event that user enters lower case a instead of upper case A
+      case 'a':
         System.out.println("Fantastic job!");
         break;
       case 'B':
+      // fall through
+      case 'b':
         System.out.println("Good job!");
         break;
       case 'C':
+      case 'c':
         System.out.println("You're doing okay.");
         break;
       case 'D':
+      case 'd':
         System.out.println("Please schedule a conference.");
         break;
       case 'F':
+      case 'f':
         System.out.println("Start finishing homework and showing up to class.");
         break;
       default:
