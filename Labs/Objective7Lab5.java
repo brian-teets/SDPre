79a09ee7da0f449d6e474b505aadd0f1720a3bd3
Brian Teets <bwt17@msn.com>
2022-03-09 12:51:55 -0700

    completed Objective7Lab5

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -5,14 +5,15 @@ public class Objective7Lab5 {
     Scanner scanner = new Scanner(System.in);
     int selection;
 
-    while(true) {   // since the loop condition is while true, inifinite loop unless user selection == 3, which should break the loop and exit the program
+    // since the loop condition is while true, inifinite loop unless user selection == 3, which should break the loop and exit the program
+    while(true) {
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
       System.out.println("2: List My favorite foods");
       System.out.println("3: Exit");
       System.out.println();
 
-      selection = scanner.nextInt();  // records and stores the user input 
+      selection = scanner.nextInt();  // records and stores the user input
 
       if (selection == 1) {
         System.out.println("Hello Human");
@@ -25,6 +26,6 @@ public class Objective7Lab5 {
         break;
       }
     }
-    scanner.close();   // the statement to close the scanner goes outside the while loop
+    scanner.close();   // the statement to close the scanner goes outside the while loop 
   }
 }
