b8d8a2eb72649e896a402d9d08b64b156ac1365d
Brian Teets <bwt17@msn.com>
2022-03-09 15:45:36 -0700

    completed Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    // TODO: write a for-loop that will print the numbers 1-10
+    for (int i = 1; i <= 10; i++) {   // increment i until i reaches value of 10
+      System.out.println(i);
+    }
+    // TODO: write a for-loop that will print the numbers 10-1
+    for (int i = 10; i >= 1; i --) {  // decrement i until i reaches value of 1
+      System.out.println(i);
+    }
+  }
+} 
