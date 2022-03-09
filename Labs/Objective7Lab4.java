4f8014a02685acb6d4f4649684126a3de34fda43
Brian Teets <bwt17@msn.com>
2022-03-09 12:16:54 -0700

    completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;  // count of 1 through 20
+    int currentSum = 1;
+
+    while (count < 20) {  // loop a total of 20 times
+      count = count + 1;  // increment the count
+      currentSum = currentSum + count;  // update current sum by adding count to sum with each iteration
+    }
+    System.out.println(currentSum);  // based on the Expected Output, only print the sum at the end of the loop
+  }
+}
