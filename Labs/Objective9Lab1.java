80a289243152cb23f85078d2952b462db4d9efab
Brian Teets <bwt17@msn.com>
2022-03-09 21:10:05 -0700

    completed Objective9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,11 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    // call the provided printName method
+    printName();
+  }
+
+  // code outside of main method, but still inside of the class
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
